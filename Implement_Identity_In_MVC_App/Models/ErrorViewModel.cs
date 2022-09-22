aa2b95e4d01f03d95e16478d650d57ec37181d7a
Akshay Yeole <akshayyeole4366@gmail.com>
2022-09-22 11:09:58 +0530

    Add project files.

---
+++ Implement_Identity_In_MVC_App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Implement_Identity_In_MVC_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
